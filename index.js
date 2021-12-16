b98e7f3815ada67519aaa174ecbc4a9795923280
Abshir J Mohamed <ajmohamed@mplspha.org>
2021-12-16 15:22:07 -0600

    JS modification  comment all pet object and function  I added detail function  and remove title name for template instead i  put in html.

--- index.js
+++ index.js
@@ -14,72 +14,112 @@
 
 
 
-const petsData = [{
-        name: "Purrsloud",
-        species: "Cat",
-        favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
-        birthYear: 2017,
-        photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
-    },
-    {
-        name: "Barksalot",
-        species: "Dog",
-        birthYear: 2008,
-        photo: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
-    },
-    {
-        name: "Meowsalot",
-        species: "Cat",
-        favFoods: ["tuna", "catnip", "celery"],
-        birthYear: 2012,
-        photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
-    }
-];
-function age(birthYear){
-    let ageCalculated = new Date().getFullYear() - birthYear;
-    if(ageCalculated === 0){
-        return `Baby`
-    }
-    else if(ageCalculated === 1){
-        return `1 year old`
-    }
-    else{
-        return `${ageCalculated} years old`
-    }
-}
-function food(foods){
-    return`
-    <ul clas="foods-list">
-    ${foods.map(function(item){
-        return`
-        <li>${item}</li>
-        `
-    }).join('')}
-    </ul>
+// const petsData = [{
+//         name: "Purrsloud",
+//         species: "Cat",
+//         favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
+//         birthYear: 2017,
+//         photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
+//     },
+//     {
+//         name: "Barksalot",
+//         species: "Dog",
+//         birthYear: 2008,
+//         photo: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
+//     },
+//     {
+//         name: "Meowsalot",
+//         species: "Cat",
+//         favFoods: ["tuna", "catnip", "celery"],
+//         birthYear: 2012,
+//         photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
+//     }
+// ];
+// function age(birthYear){
+//     let ageCalculated = new Date().getFullYear() - birthYear;
+//     if(ageCalculated === 0){
+//         return `Baby`
+//     }
+//     else if(ageCalculated === 1){
+//         return `1 year old`
+//     }
+//     else{
+//         return `${ageCalculated} years old`
+//     }
+// }
+// function food(foods){
+//     return`
+//     <ul clas="foods-list">
+//     ${foods.map(function(item){
+//         return`
+//         <li>${item}</li>
+//         `
+//     }).join('')}
+//     </ul>
     
-    `
+//     `
 
-};
+// };
 
-function petTemplate(pet){
-    return `
-    <div class="animal">
-    <img class="pet-photo" src="${pet.photo}">
-    <h2 class="pet-name">${pet.name} <span class="species">(${pet.species})</span></h2>
-    <p><strong>Age: </strong> ${age(pet.birthYear)}</p>
-    ${pet.favFoods ? food(pet.favFoods) : ''}
+// function petTemplate(pet){
+//     return `
+//     <div class="animal">
+//     <img class="pet-photo" src="${pet.photo}">
+//     <h2 class="pet-name">${pet.name} <span class="species">(${pet.species})</span></h2>
+//     <p><strong>Age: </strong> ${age(pet.birthYear)}</p>
+//     ${pet.favFoods ? food(pet.favFoods) : ''}
 
-    </div>
-    `
-}
+//     </div>
+//     `
+// }
 
-//   document.getElementById("app").innerHTML = `Hello, two plus two equals ${2 + 2}
-//   <p>Hi There.<\p>
-//   <p>Hello Again</p>`
+// //   document.getElementById("app").innerHTML = `Hello, two plus two equals ${2 + 2}
+// //   <p>Hi There.<\p>
+// //   <p>Hello Again</p>`
 
-//`` template is string with powerful
+// //`` template is string with powerful
 
-document.getElementById("app").innerHTML = `
-  <h1 class="app-title"> Pets (${petsData.length} results)</h1>
-  ${petsData.map(petTemplate).join('')}
-  <p class="footer">these ${petsData.length} pets were added recently. check back soon for updates</P>`
+// document.getElementById("app").innerHTML = `
+//   <h1 class="app-title"> Pets (${petsData.length} results)</h1>
+//   ${petsData.map(petTemplate).join('')}
+//   <p class="footer">these ${petsData.length} pets were added recently. check back soon for updates</P>`
+
+  // array of peole name and positiom
+
+  const titles = [
+      {
+          name : "Joyce",
+          job: 'Property Managment',
+          phone: "612-821-2111",
+          company: "MPHA"
+
+
+      },
+      {
+          name: "Abshir",
+          job: "Managment Aide",
+          photo:  "/img/abk.jpg",
+          phone : "612-821-2101",
+          company: "MPHA"
+      }
+  ]
+
+  // templete function for arrayMap
+  const detail = (person) => {
+      return `
+      <div class='animal person'>
+      <img class="pet-photo" src="${person.photo ? person.photo : ""}">  
+      <h2 class='pet-name person '> ${person.name}</h2>
+      <h3 class="job:>Company: ${person.company}</h3>
+      <h3 class ="job">Title: ${person.job}</h3>
+      <h3 class="job">Phone: ${person.phone}</h3>
+      
+      </div>
+      `
+  };
+
+  // get dom html
+  document.getElementById("person").innerHTML = `
+${titles.map(detail).join('')}
+  <footer class="footer">These ${titles.length} were added recently</footer>
+  `
